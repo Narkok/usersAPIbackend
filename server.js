b65dd8e75482cf2ff87a91f5bdaccc69616edace
Narek Stepanyan <narek.stepanyan@ramax.ru>
2019-10-01 19:43:21 +0300

    Переезд

---
+++ server.js
@@ -0,0 +1,18 @@
+const express        = require('express');
+const MongoClient    = require('mongodb').MongoClient;
+const bodyParser     = require('body-parser');
+const db             = require('./config/db');
+const app            = express();
+
+const port = 8000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+MongoClient.connect(db.url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+  if (err) return console.log(err)
+  var database = client.db("nrkkdb")
+  require('./app/routes')(app, database);
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });
+})
