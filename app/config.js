fb5e044c8446a48c3c779e9f781715dfe9ee3e40
Stepanyan Narek <nar-ste@yandex.ru>
2020-07-22 23:15:42 +0300

    Update config.js

--- app/config.js
+++ app/config.js
@@ -5,7 +5,6 @@ class Config {
 
     constructor() {
         /// Адрес БД
-        // this.url = "mongodb+srv://" + config.login + ":" + config.password + "@cluster0-wlutl.mongodb.net/nrkkdb?retryWrites=true&w=majority"
         this.url = "mongodb+srv://" + serverConfig.login + ":" + serverConfig.password + "@cluster0-wlutl.mongodb.net/nrkkdb?retryWrites=true&w=majority"
 
         /// Название БД
